1c53069c3968c6447407438cbb9817e9dc1dbdf5
maybecake <maybemaybecake@gmail.com>
2025-04-21 05:12:53 +0000

    Try to add a go grpc server.

---
+++ service/simple/hello.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+	pb "github.com/maybecake/stacks/api/protos/hello"
+)
+
+type server struct {
+	pb.UnimplementedHelloServiceServer
+}
+
+func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloResponse, error) {
+	return &pb.HelloResponse{Message: "Hello, " + in.Name + "!"}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterHelloServiceServer(s, &server{})
+	log.Printf("server listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
