059a00b794c0e8459402f52e123717345051303e
maybecake <maybemaybecake@gmail.com>
2025-04-27 15:33:36 +0000

    scrollable

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import './App.css'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import { Home, Samples } from '@features/index'
-import { Header } from '@components/Header'
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Home, Samples } from "@features/index";
+import { Header } from "@components/Header";
 
 function App() {
   return (
@@ -10,6 +10,23 @@ function App() {
       <div className="app">
         <Header />
         <main className="main-content">
+          <div className="main-content-inner">
+            <div className="scrollable">
+              <p> a lot of content in here </p>
+              <p> a lot of content in here </p>
+              <p> a lot of content in here </p>
+              <p> a lot of content in here </p>
+              <p> a lot of content in here </p>
+              <p> a lot of content in here </p>
+              <p> a lot of content in here </p>
+              <p> a lot of content in here </p>
+              <p> a lot of content in here </p>
+              <p> a lot of content in here </p>
+              <p> a lot of content in here </p>
+              <p> a lot of content in here </p>
+              <p> a lot of content in here </p>
+            </div>
+          </div>
           <Switch>
             <Route exact path="/" component={Home} />
             <Route path="/samples" component={Samples} />
@@ -17,8 +34,7 @@ function App() {
         </main>
       </div>
     </Router>
-  )
+  );
 }
 
-export default App
-
+export default App;
