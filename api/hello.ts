76b938ac3246584b14ce0b8d0235fb7f1fe371f7
maybecake <maybemaybecake@gmail.com>
2026-04-05 17:39:31 +0000

    restore comment.

--- api/hello.ts
+++ api/hello.ts
@@ -1,6 +1,7 @@
 import { ConnectRouter } from "@connectrpc/connect";
 import { connectNodeAdapter } from "@connectrpc/connect-node";
 import { create } from "@bufbuild/protobuf";
+// These imports resolve after `buf generate` runs at build time
 import { HelloService, HelloResponseSchema } from "./.gen/hello_pb.js";
 import { greetHello } from "../lib/domain/greeting";
 import { PostgresGreetingStore } from "../lib/adapters/postgres/greeting_store";
